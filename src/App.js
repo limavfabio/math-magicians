236a010f2372927a296d8da15826a8a6b6d4699a
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-06 19:44:00 -0300

    Fix design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
 import './App.css';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <main>
-      <Calculator />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Calculator />
+      </main>
+    );
+  }
 }
 
 export default App;
