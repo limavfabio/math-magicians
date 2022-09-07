bf1b31c9278700f54574f8c2c13213e046da935b
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-07 16:59:16 -0300

    Add logic
    This commit adds the calculator logic,
    making the app functions as a calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,96 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import '../styles/Calculator.css';
+/* eslint-disable no-unused-vars */
+import calculate from '../logic/Calculate';
+import operate from '../logic/Operate';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (e) => {
+    const result = calculate(this.state, e.target.textContent);
+    this.setState(result);
+  };
+
   render() {
+    const { total, next } = this.state;
     return (
       <section className="calculator-body">
         {/* result Row */}
-        <div className="result">9876543210</div>
+        <div className="result">{next || total}</div>
 
         {/* First Row */}
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button className="operator" type="button">
+        <button onClick={this.handleClick} type="button">
+          AC
+        </button>
+        <button onClick={this.handleClick} type="button">
+          +/-
+        </button>
+        <button onClick={this.handleClick} type="button">
+          %
+        </button>
+        <button onClick={this.handleClick} className="operator" type="button">
           /
         </button>
 
         {/* Second Row */}
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button className="operator" type="button">
+        <button onClick={this.handleClick} type="button">
+          7
+        </button>
+        <button onClick={this.handleClick} type="button">
+          8
+        </button>
+        <button onClick={this.handleClick} type="button">
+          9
+        </button>
+        <button onClick={this.handleClick} className="operator" type="button">
           *
         </button>
 
         {/* Third Row */}
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button className="operator" type="button">
+        <button onClick={this.handleClick} type="button">
+          4
+        </button>
+        <button onClick={this.handleClick} type="button">
+          5
+        </button>
+        <button onClick={this.handleClick} type="button">
+          6
+        </button>
+        <button onClick={this.handleClick} className="operator" type="button">
           -
         </button>
 
         {/* Fourth Row */}
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button className="operator" type="button">
+        <button onClick={this.handleClick} type="button">
+          1
+        </button>
+        <button onClick={this.handleClick} type="button">
+          2
+        </button>
+        <button onClick={this.handleClick} type="button">
+          3
+        </button>
+        <button onClick={this.handleClick} className="operator" type="button">
           +
         </button>
 
         {/* Fifth Row */}
-        <button className="btn-0" type="button">
+        <button onClick={this.handleClick} className="btn-0" type="button">
           0
         </button>
-        <button type="button">.</button>
-        <button className="operator" type="button">
+        <button onClick={this.handleClick} type="button">
+          .
+        </button>
+        <button onClick={this.handleClick} className="operator" type="button">
           =
         </button>
       </section>
