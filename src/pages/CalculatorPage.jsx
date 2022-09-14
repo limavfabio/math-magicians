fd569c4df753edb29dd1630a5e5c2c08e2062430
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-14 13:24:41 -0300

    Implement Routing

---
+++ src/pages/CalculatorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+import './CalculatorPage.css';
+
+const CalculatorPage = () => (
+  <div className="calculator-page">
+    <h1>Lets do Math!</h1>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
